cc7ab8ccabbf19e968846c2216d5746a8c829dcf
bearmug <pavel.fadeev@gmail.com>
2019-01-24 02:14:12 +0300

    add implistic migrations logic

--- src/db_migrations.erl
+++ src/db_migrations.erl
@@ -2,6 +2,11 @@
 
 -export([migrate/1]).
 
+-type version()   :: 0..1000.
+-type filename()  :: nonempty_string().
+-type folder()    :: nonempty_string().
+-type migration() :: {version(), filename()}.
+
 -spec migrate(string()) -> ok.
 migrate(ScriptsLocation) ->
   case find_migrations(ScriptsLocation) of
@@ -9,20 +14,37 @@ migrate(ScriptsLocation) ->
     Migrations -> db_tx(fun() ->
                           ok = db_init(),
                           [do_migration(M) || M <- Migrations]
-                        end)a
+                        end)
   end.
 
-find_migrations() ->
-  erlang:error(not_implemented).
-
 db_tx(Fun) ->
-  erlang:error(not_implemented).
+  query("open db-level locking transaction"),
+  try Fun() of
+    _ ->
+      query("commit transaction")
+  catch
+    E ->
+      query("rollback transaction"),
+      throw(E)
+  end.
 
 db_init() ->
-  erlang:error(not_implemented).
+  query("create migrations table if not exists").
 
-do_migration(M) ->
-  erlang:error(not_implemented).
+do_migration({Version, FileName}) ->
+  Version = next_migration_version(),
+  query("run migration body"),
+  query("insert migration version with Version and FileName").
 
+-spec find_migrations(folder()) -> [migration()].
 find_migrations(ScriptsLocation) ->
+  case file:list_dir_all(ScriptsLocation) of
+    [] -> [];
+    Files -> [string:split(F, "_") || F <- Files]
+  end.
+
+query(Str) ->
   erlang:error(not_implemented).
+
+next_migration_version() ->
+  query("find latest migration version") + 1.
