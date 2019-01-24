b66f71184e70d81ab3a8fb031b79aecebac903dc
bearmug <pavel.fadeev@gmail.com>
2019-01-25 00:41:25 +0300

    generate dummy tests

--- test/db_migrations_epgsql_test.erl
+++ test/db_migrations_epgsql_test.erl
@@ -2,5 +2,12 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-simple_test() ->
-  ?assert(true).
+empty_folder_test() -> ?assert(true).
+wrong_filename_test() -> ?assert(true).
+wrong_path_test() -> ?assert(true).
+regular_upgrade_test() -> ?assert(true).
+rewrite_version_test() -> ?assert(true).
+faulty_script_test() -> ?assert(true).
+start_not_from_zero_test() -> ?assert(true).
+versions_gap_test() -> ?assert(true).
+negative_version_test() -> ?assert(true).
