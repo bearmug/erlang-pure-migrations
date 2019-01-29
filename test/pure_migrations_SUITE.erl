094420def7deb082450a4ad6628c8e279925eac8
bearmug <pavel.fadeev@gmail.com>
2019-01-30 01:43:02 +0300

    refactor test

--- test/pure_migrations_SUITE.erl
+++ test/pure_migrations_SUITE.erl
@@ -29,101 +29,68 @@ wrong_filename_test(Opts) ->
   PreparedCall = engine:migrate(
     filename:join([?config(data_dir, Opts), "01-invalid-script-name"]),
     fun(F) -> F() end,
-    fun(Q) -> case Q of
-                "CREATE" ++ _Tail -> ok;
-                "SELECT version" ++ _Tail -> [];
-                "SELECT max" ++ _Tail -> -1;
-                "INSERT"++_Tail -> throw("Insert calls not expected");
-                true -> throw("Unknown calls not expected")
-              end
-    end),
+    query_fun([], ok)
+  ),
   ?assertError(badarg, PreparedCall()).
 
 regular_upgrade_test(Opts) ->
   PreparedCall = engine:migrate(
     filename:join([?config(data_dir, Opts), "02-regular-upgrade"]),
     fun(F) -> F() end,
-    fun(Q) -> case Q of
-                "CREATE" ++ _Tail -> ok;
-                <<"CREATE TABLE fruit (\n name TEXT\n);">> -> ok;
-                "SELECT version" ++ _Tail -> [];
-                "SELECT max" ++ _Tail -> -1;
-                "INSERT"++_Tail -> ok;
-                true -> throw("Unknown calls not expected")
-              end
-    end),
+    query_fun([], ok)
+  ),
   ?assertEqual([ok], PreparedCall()).
 
 rewrite_version_test(Opts) ->
   PreparedCall = engine:migrate(
     filename:join([?config(data_dir, Opts), "03-rewrite-version"]),
     fun(F) -> F() end,
-    fun(Q) -> case Q of
-                "CREATE" ++ _Tail -> ok;
-                "SELECT version" ++ _Tail -> [{0, "00_very_first_script.sql"}];
-                "SELECT max" ++ _Tail -> 0;
-                "INSERT"++_Tail -> ok;
-                true -> throw("Unknown calls not expected")
-              end
-    end),
+    query_fun([{0, "00_very_first_script.sql"}], ok)
+  ),
   ?assertError({badmatch, 1}, PreparedCall()).
 
 faulty_script_test(Opts) ->
   PreparedCall = engine:migrate(
     filename:join([?config(data_dir, Opts), "00-default"]),
     fun(F) -> F() end,
-    fun(Q) -> case Q of
-                "CREATE" ++ _Tail -> ok;
-                <<"CREATE TABLE fruit (\n name TEXT\n);">> ->
-                  {error, system_fault, "Please check database connectivity"};
-                "SELECT version" ++ _Tail -> [];
-                "SELECT max" ++ _Tail -> -1;
-                "INSERT"++_Tail -> ok;
-                true -> throw("Unknown calls not expected")
-              end
-    end),
+    query_fun([], {error, system_fault, "Please check database connectivity"})
+  ),
   ?assertError({badmatch, {
-      error,system_fault, "Please check database connectivity"
+    error, system_fault, "Please check database connectivity"
   }}, PreparedCall()).
 
 start_not_from_zero_test(Opts) ->
   PreparedCall = engine:migrate(
     filename:join([?config(data_dir, Opts), "04-invalid-start-version"]),
     fun(F) -> F() end,
-    fun(Q) -> case Q of
-                "CREATE" ++ _Tail -> ok;
-                "SELECT version" ++ _Tail -> [];
-                "SELECT max" ++ _Tail -> -1;
-                "INSERT"++_Tail -> ok;
-                true -> throw("Unknown calls not expected")
-              end
-    end),
+    query_fun([], ok)
+  ),
   ?assertError({badmatch, 0}, PreparedCall()).
 
 versions_gap_test(Opts) ->
   PreparedCall = engine:migrate(
     filename:join([?config(data_dir, Opts), "04-invalid-start-version"]),
     fun(F) -> F() end,
-    fun(Q) -> case Q of
-                "CREATE" ++ _Tail -> ok;
-                "SELECT version" ++ _Tail -> [{0, "00_very_first_script.sql"}];
-                "SELECT max" ++ _Tail -> 0;
-                "INSERT"++_Tail -> ok;
-                true -> throw("Unknown calls not expected")
-              end
-    end),
+    query_fun([{0, "00_very_first_script.sql"}], ok)
+  ),
   ?assertError({badmatch, 1}, PreparedCall()).
 
 negative_version_test(Opts) ->
   PreparedCall = engine:migrate(
     filename:join([?config(data_dir, Opts), "05-negative-version"]),
     fun(F) -> F() end,
-    fun(Q) -> case Q of
-                "CREATE" ++ _Tail -> ok;
-                "SELECT version" ++ _Tail -> [];
-                "SELECT max" ++ _Tail -> -1;
-                "INSERT"++_Tail -> ok;
-                true -> throw("Unknown calls not expected")
-              end
-    end),
+    query_fun([], ok)
+  ),
   ?assertError({badmatch, 0}, PreparedCall()).
+
+query_fun(ExistingVersions, MigrationResponse) ->
+  fun(Q) ->
+    case Q of
+      "CREATE" ++ _Tail -> ok;
+      "SELECT version" ++ _Tail -> ExistingVersions;
+      "SELECT max" ++ _Tail -> length(ExistingVersions) - 1;
+      "INSERT" ++ _Tail -> ok;
+      <<"CREATE TABLE fruit (\n name TEXT\n);">> -> MigrationResponse;
+      true -> throw("Unknown calls not expected")
+    end
+  end.
