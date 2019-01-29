a55555972e095ddb3c5ac7e6fb700ceb501ace20
Pavel Fadeev <pavel.fadeev@gmail.com>
2019-01-29 23:54:33 +0100

    Contribute unit level tests (#4)
    * refactor project structure a little
    
    * use traditional (for erlang) partial application technique
    
    * make common test read data
    
    * adjust test cycle
    
    * update test
    
    * make tests work
    
    * make tests work
    
    * even more unit tests
    
    * all unit tests arein place
    
    * refactor test
    
    * less code in test module

--- src/engine.erl
+++ src/engine.erl
@@ -7,7 +7,7 @@
 -spec migrate(
     Path :: nonempty_string(),
     FTx :: fun((F) -> ok | error()),
-    FQuery :: fun((nonempty_string()) -> ok | list(string()) | error())) -> R
+    FQuery :: fun((nonempty_string()) -> ok | list(string()) | integer() | error())) -> R
   when
   F :: fun(() -> ok | error()),
   R :: fun(() -> ok | error()).
@@ -16,13 +16,13 @@ migrate(Path, FTx, FQuery) ->
   fun() -> FTx(flatten(
     map(
       find_migrations(Path, FQuery),
-      partial(fun do_migration/3, Path, FQuery)),
+      fun({V, F}) -> do_migration(Path, FQuery, {V, F}) end),
     fun(M)-> ok = M() end))
   end.
 
 do_migration(Path, FQuery, {Version, FileName}) ->
   Version = FQuery(dialect_postgres:latest_existing_version()) + 1,
-  ScriptPath = Path ++ "/" ++ FileName,
+  ScriptPath = filename:join(Path, FileName),
   compose(
     fun() -> file:read_file(ScriptPath) end,
     fun({ok, ScriptBody}) ->
@@ -37,9 +37,12 @@ find_migrations(ScriptsLocation, FQuery) ->
     fun() -> file:list_dir_all(ScriptsLocation) end,
     fun({ok, Files}) -> lists:filter(
         fun(N) -> not sets:is_element(N, MigrationsDone) end,
-        lists:keysort(1, [string:to_integer(string:split(F, "_")) || F <- Files]))
+        lists:keysort(1, [
+          {list_to_integer(head(string:split(F, "_"))), F} || F <- Files]))
     end
   ).
+
+head([H|_]) -> H.
 
 compose(F1, F2) ->
   fun() -> F2(F1()) end.
@@ -49,6 +52,3 @@ map(Generate, Map) ->
 
 flatten(Generate, Flatten) ->
   fun() -> [Flatten(R) || R <- Generate()] end.
-
-partial(F, A, B) ->
-  fun(C) -> F(A, B, C) end.
