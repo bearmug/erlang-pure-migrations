6be0969e795b164e93a01790208505afa840d407
bearmug <pavel.fadeev@gmail.com>
2019-02-02 17:24:42 +0300

    #6 two step migration test

--- test/epgsql_migrations_SUITE.erl
+++ test/epgsql_migrations_SUITE.erl
@@ -8,7 +8,8 @@
 
 all() -> [
           migrate_one_script_test,
-          migrate_few_scripts_test
+          migrate_few_scripts_test,
+          incremental_migration_test
          ].
 
 migrate_one_script_test(Opts) ->
@@ -43,37 +44,42 @@ migrate_few_scripts_test(Opts) ->
        epgsql:squery(Conn, "select count(*) from fruit where color = 'yellow'")).
 
 incremental_migration_test(Opts) ->
-  Conn = ?config(conn, Opts),
-  TxFun =
-    fun(F) ->
-    epgsql:with_transaction(Conn, fun(_) -> F() end)
-          end,
-  MigrationStep1 = engine:migrate(
-    filename:join([?config(data_dir, Opts), "00-single-script-test"]),
-    TxFun, epgsql_query_fun(Conn)
-  ),
-  MigrationStep2 = engine:migrate(
-    filename:join([?config(data_dir, Opts), "01-two-scripts-test"]),
-    TxFun, epgsql_query_fun(Conn)
-  ),
+    Conn = ?config(conn, Opts),
+    TxFun =
+        fun(F) ->
+                epgsql:with_transaction(Conn, fun(_) -> F() end)
+        end,
+    MigrationStep1 = engine:migrate(
+                       filename:join([?config(data_dir, Opts), "00-single-script-test"]),
+                       TxFun, epgsql_query_fun(Conn)
+                      ),
+    MigrationStep2 = engine:migrate(
+                       filename:join([?config(data_dir, Opts), "01-two-scripts-test"]),
+                       TxFun, epgsql_query_fun(Conn)
+                      ),
 
-  %% assert migrations table created and nothing done
+    %% assert migrations table created and nothing done
+    ?assertMatch(
+       {ok, _,[{null}]},
+       epgsql:squery(Conn, "select max(version) from database_migrations_history")),
+    ?assertMatch(
+       {error,{error, _, _, undefined_table, <<"relation \"fruit\" does not exist">>, _}},
+       epgsql:squery(Conn, "select count(*) from fruit")),
 
+    %% assert step 1 migration
+    ok = MigrationStep1(),
+    ?assertMatch(
+       {ok, _,[{<<"0">>}]},
+       epgsql:squery(Conn, "select max(version) from database_migrations_history")),
 
-  %% assert step 1 migration
-  ok = MigrationStep1(),
-  ?assertMatch(
-    {ok, _,[{<<"0">>}]},
-    epgsql:squery(Conn, "select max(version) from database_migrations_history")),
-
-  %% assert step 2 migration
-  ok =MigrationStep2(),
-  ?assertMatch(
-    {ok, _,[{<<"1">>}]},
-    epgsql:squery(Conn, "select max(version) from database_migrations_history")),
-  ?assertMatch(
-    {ok, _,[{<<"1">>}]},
-    epgsql:squery(Conn, "select count(*) from fruit where color = 'yellow'")).
+    %% assert step 2 migration
+    ok =MigrationStep2(),
+    ?assertMatch(
+       {ok, _,[{<<"1">>}]},
+       epgsql:squery(Conn, "select max(version) from database_migrations_history")),
+    ?assertMatch(
+       {ok, _,[{<<"1">>}]},
+       epgsql:squery(Conn, "select count(*) from fruit where color = 'yellow'")).
 
 epgsql_query_fun(Conn) ->
     fun(Q) ->
@@ -83,10 +89,11 @@ epgsql_query_fun(Conn) ->
                 ok -> ok;
                 {ok, [
                       {column, <<"version">>, _, _, _, _, _},
-                      {column, <<"filename">>, _, _, _, _, _}], Data} -> Data;
+                      {column, <<"filename">>, _, _, _, _, _}], Data} ->
+                    [{list_to_integer(binary_to_list(BinV)), binary_to_list(BinF)} || {BinV, BinF} <- Data];
                 {ok, [{column, <<"max">>, _, _, _, _, _}], [{null}]} -> -1;
                 {ok, [{column, <<"max">>, _, _, _, _, _}], [{N}]} ->
-                    list_to_integer(binary:bin_to_list(N));
+                    list_to_integer(binary_to_list(N));
                 [{ok, _, _}, {ok, _}] -> ok;
                 {ok, _, _} -> ok;
                 {ok, _} -> ok;
