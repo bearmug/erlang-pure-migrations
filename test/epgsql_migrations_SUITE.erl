69e2de1d918582aac6e7e27b0697af7bd979e96f
bearmug <pavel.fadeev@gmail.com>
2019-02-02 15:02:32 +0300

    #6 configure dummy test with connection

--- test/epgsql_migrations_SUITE.erl
+++ test/epgsql_migrations_SUITE.erl
@@ -1,12 +1,33 @@
 -module(epgsql_migrations_SUITE).
 
+-include_lib("epgsql/include/epgsql.hrl").
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 -compile(export_all).
 
 all() -> [
-  dummy_test
-].
+          dummy_test
+         ].
 
-dummy_test(_Opts) -> ok.
+init_per_testcase(_TestCase, Opts) ->
+    {ok, [{host, Host},
+          {port, _Port},
+          {database, Database},
+          {username, Username},
+          {secret, Secret},
+          {timeout, Timeout}]} = application:get_env(db, config),
+    {ok, C} = epgsql:connect(Host, Username, Secret,
+                             #{
+                               database => Database,
+                               timeout => Timeout
+                              }),
+    [{conn, C}|Opts].
+
+end_per_testcase(_TestCase, Opts) ->
+    ok = epgsql:close(?config(conn, Opts)).
+
+dummy_test(Opts) ->
+    C = ?config(conn, Opts),
+    io:format("tests connection: ~p~n", [C]),
+    ok.
