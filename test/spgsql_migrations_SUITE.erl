8b03a8caabfa3b254b3996b1cdb1bd840fc7db9c
bearmug <pavel.fadeev@gmail.com>
2019-02-06 01:06:09 +0300

    #8 one more running test

--- test/spgsql_migrations_SUITE.erl
+++ test/spgsql_migrations_SUITE.erl
@@ -7,7 +7,7 @@
 
 all() -> [ migrate_one_script_test
          , migrate_few_scripts_test
-           %%         , incremental_migration_test
+         , incremental_migration_test
            %%         , wrong_initial_version_test
            %%         , migration_gap_test
            %%         , transactional_migration_test
@@ -39,45 +39,41 @@ migrate_few_scripts_test(Opts) ->
     ?assertMatch(
        {{select, 1}, [{1}]},
        pgsql_connection:simple_query("select count(*) from fruit where color = 'yellow'", Conn)).
-%%
-%%incremental_migration_test(Opts) ->
-%%    Conn = ?config(conn, Opts),
-%%    TxFun =
-%%        fun(F) ->
-%%                epgsql:with_transaction(Conn, fun(_) -> F() end)
-%%        end,
-%%    MigrationStep1 = engine:migrate(
-%%                       filename:join([?config(data_dir, Opts), "00-single-script-test"]),
-%%                       TxFun, epgsql_query_fun(Conn)
-%%                      ),
-%%    MigrationStep2 = engine:migrate(
-%%                       filename:join([?config(data_dir, Opts), "01-two-scripts-test"]),
-%%                       TxFun, epgsql_query_fun(Conn)
-%%                      ),
-%%
-%%    %% assert migrations table created and nothing done
-%%    ?assertMatch(
-%%       {ok, _,[{null}]},
-%%       epgsql:squery(Conn, "select max(version) from database_migrations_history")),
-%%    ?assertMatch(
-%%       {error,{error, _, _, undefined_table, <<"relation \"fruit\" does not exist">>, _}},
-%%       epgsql:squery(Conn, "select count(*) from fruit")),
-%%
-%%    %% assert step 1 migration
-%%    ok = MigrationStep1(),
-%%    ?assertMatch(
-%%       {ok, _, [{<<"0">>}]},
-%%       epgsql:squery(Conn, "select max(version) from database_migrations_history")),
-%%
-%%    %% assert step 2 migration
-%%    ok =MigrationStep2(),
-%%    ?assertMatch(
-%%       {ok, _, [{<<"1">>}]},
-%%       epgsql:squery(Conn, "select max(version) from database_migrations_history")),
-%%    ?assertMatch(
-%%       {ok, _, [{<<"1">>}]},
-%%       epgsql:squery(Conn, "select count(*) from fruit where color = 'yellow'")).
-%%
+
+incremental_migration_test(Opts) ->
+    Conn = ?config(conn, Opts),
+    TxFun = spgsql_tx_fun(),
+    QueryFun = spgsql_query_fun(Conn),
+    MigrationStep1 = engine:migrate(
+                       filename:join([?config(data_dir, Opts), "00-single-script-test"]),
+                       TxFun, QueryFun),
+    MigrationStep2 = engine:migrate(
+                       filename:join([?config(data_dir, Opts), "01-two-scripts-test"]),
+                       TxFun, QueryFun),
+
+    %% assert migrations table created and nothing done
+    ?assertMatch(
+       {{select, 1}, [{null}]},
+       pgsql_connection:simple_query("select max(version) from database_migrations_history", Conn)),
+    ?assertMatch(
+       {error, {pgsql_error, [_, _, _, {message, <<"relation \"fruit\" does not exist">>}|_]}},
+       pgsql_connection:simple_query("select count(*) from fruit", Conn)),
+
+    %% assert step 1 migration
+    ok = MigrationStep1(),
+    ?assertMatch(
+       {{select, 1}, [{0}]},
+       pgsql_connection:simple_query("select max(version) from database_migrations_history", Conn)),
+
+    %% assert step 2 migration
+    ok =MigrationStep2(),
+    ?assertMatch(
+       {{select, 1}, [{1}]},
+       pgsql_connection:simple_query("select max(version) from database_migrations_history", Conn)),
+    ?assertMatch(
+       {{select, 1}, [{1}]},
+       pgsql_connection:simple_query("select count(*) from fruit where color = 'yellow'", Conn)).
+
 %%wrong_initial_version_test(Opts) ->
 %%    Conn = ?config(conn, Opts),
 %%    PreparedCall = engine:migrate(
@@ -149,10 +145,8 @@ spgsql_query_fun(Conn) ->
             io:format("query=~p, res=~p~n", [Q, Res]),
             case Res of
                 {{select, 0}, []} -> [];
-                {ok, [
-                      {column, <<"version">>, _, _, _, _, _},
-                      {column, <<"filename">>, _, _, _, _, _}], Data} ->
-                    [{list_to_integer(binary_to_list(BinV)), binary_to_list(BinF)} || {BinV, BinF} <- Data];
+                {{select, 1}, Data = [{_V, _F}|_]}  ->
+                    [{V, binary_to_list(BinF)} || {V, BinF} <- Data];
                 {{select, 1}, [{null}]} -> -1;
                 {{select, 1}, [{N}]} -> N;
                 {{insert, 0, 1}, []} -> ok;
